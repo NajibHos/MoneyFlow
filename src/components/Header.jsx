e9542fa140b73d80a8ca2416c8279361892a4e1c
Najib Hossain <najibhossn@gmail.com>
2025-08-20 15:34:19 +0600

    Update header UI and add theme toggle

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,52 +1,62 @@
 import { useAuth } from "../utils/AuthProvider";
 import { Link } from "react-router-dom";
-import { Github, House, LogIn, LogOut } from "lucide-react";
+import { House, LogIn, LogOut, Moon, Sun } from "lucide-react";
+import { useTheme } from "../utils/ThemeProvider";
 
 const Header = () => {
 
   const { user, logOutUser } = useAuth();
+  const { theme, toggleTheme } = useTheme();
+  console.log(theme)
 
   return (
-     <div className="h-[10vh] w-full flex justify-center
-     items-center">
+     <div className="h-auto w-full py-5 flex justify-center items-center">
       <div className="h-full w-[90%] flex justify-between items-center">
         <div className="h-auto w-[40%] flex justify-start items-center">
           <Link to='/'>
-          <h2 className="text-3xl text-white font-semibold
-           cursor-pointer font-headings">
+          <h2 className="text-3xl font-headings font-medium
+            cursor-pointer text-zinc-900 dark:text-white"
+          >
             Moneyflow
           </h2>
           </Link>
         </div>
-        <div className="h-auto w-[40%] flex justify-end items-center gap-2">
+        <div className="h-auto w-[40%] flex justify-end items-center gap-5 md:gap-5">
           {
+            theme === 'light' && <button 
+              onClick={toggleTheme}
+              className="cursor-pointer text-zinc-900"
+            >
+              <Sun size={22} />
+            </button>
+          }
+          {
+            theme === 'dark' && <button 
+              onClick={toggleTheme}
+              className="cursor-pointer text-white"
+            >
+              <Moon size={22} />
+            </button>
+          }
+            {
             !user ? (
-              <>
-              <a href="https://github.com/NajibHos/MoneyFlow"
-               target="_blank"
-               className="text-lg text-zinc-200 cursor-pointer
-               hover:text-white px-4 py-2 bg-transparent
-               hover:bg-zinc-800 rounded-xl">
-                <Github  />
-              </a>
-
+              <> 
               {/* 2 different buttons for smaller and larger screens */}
               <Link to='/sign-in'>
-              <button type="button" className="block md:hidden
-                text-lg text-zinc-200 font-semibold cursor-pointer
-                font-descriptions
-               hover:text-white px-4 py-2 bg-transparent
-               hover:bg-zinc-800 rounded-xl">
-                <LogIn />
+              <button 
+                className="block md:hidden cursor-pointer text-zinc-900 
+                dark:text-white hover:text-blue-600"
+              >
+                <LogIn size={22} />
               </button>
               </Link>
 
               <Link to='/sign-in'>
-              <button type="button" className="hidden md:block
-                text-lg text-zinc-200 font-semibold cursor-pointer
-                font-descriptions
-               hover:text-white px-4 py-2 bg-transparent
-               hover:bg-zinc-800 rounded-xl">
+              <button 
+                className="hidden md:block
+                text-lg font-descriptions font-semibold cursor-pointer
+                text-zinc-900 dark:text-white hover:text-blue-600"
+              >
                 Sign in
               </button>
               </Link>
@@ -55,44 +65,37 @@ const Header = () => {
               <>
                 {/* 2 different buttons for smaller and larger screens */}
                 <Link to='/dashboard'>
-                <button type="button"
-                  className="block md:hidden
-                  text-lg text-zinc-200
-                  font-semibold cursor-pointer font-descriptions
-                 hover:text-white px-4 py-2 bg-transparent
-                 hover:bg-zinc-800 rounded-xl">
-                  <House />
+                <button
+                  className="block md:hidden cursor-pointer text-zinc-900 
+                  dark:text-white hover:text-blue-600"
+                >
+                  <House size={22} />
                 </button>
                 </Link>
 
                 <Link to='/dashboard'>
-                <button type="button"
+                <button
                   className="hidden md:block
-                  text-lg text-zinc-200
-                  font-semibold cursor-pointer font-descriptions
-                 hover:text-white px-4 py-2 bg-transparent
-                 hover:bg-zinc-800 rounded-xl">
+                  text-lg font-descriptions font-semibold cursor-pointer
+                  text-zinc-900 dark:text-white hover:text-blue-600">
                   Dashboard
                 </button>
                 </Link>
 
-                <button type="button"
-                  className="block md:hidden
-                  text-lg text-zinc-200
-                  font-semibold cursor-pointer font-descriptions
-                 hover:text-white px-4 py-2 bg-transparent
-                 hover:bg-zinc-800 rounded-xl"
-                  onClick={logOutUser}>
-                  <LogOut />
+                <button 
+                  className="block md:hidden cursor-pointer text-zinc-900 
+                  dark:text-white hover:text-blue-600"
+                  onClick={logOutUser}
+                >
+                  <LogOut size={22} />
                 </button>
 
-                <button type="button"
+                <button
+                  onClick={logOutUser}
                   className="hidden md:block
-                  text-lg text-zinc-200
-                  font-semibold cursor-pointer font-descriptions
-                 hover:text-white px-4 py-2 bg-transparent
-                 hover:bg-zinc-800 rounded-xl"
-                  onClick={logOutUser}>
+                  text-lg font-descriptions font-semibold cursor-pointer
+                  text-zinc-900 dark:text-white hover:text-blue-600"
+                >
                   Sign Out
                 </button>
               </>
