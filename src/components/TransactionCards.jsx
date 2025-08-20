c2828abb6f4324e413931fc14fedccae058c6940
Najib Hossain <najibhossn@gmail.com>
2025-08-20 15:21:46 +0600

    Add theme style and update card ui

--- src/components/TransactionCards.jsx
+++ src/components/TransactionCards.jsx
@@ -1,44 +1,57 @@
-import { Trash2 } from "lucide-react";
 
 const TransactionCards = ({ data, removeTransaction }) => {
 
   return (
-    <div className="h-[150px] w-full flex flex-col justify-center items-center
-    gap-5 bg-zinc-950 p-6 border rounded border-zinc-900">
-      <div className="h-auto w-full text-left">
-        <h2 className="text-base text-zinc-200 font-descriptions font-medium">
+    <div className="h-auto w-full p-6 flex flex-col justify-center items-center
+      gap-4 rounded bg-card-light dark:bg-card-dark"
+    >
+      <div className="h-auto w-full flex justify-between items-center">
+        <div className="h-auto w-[70%] text-left">
+        <h2 className="text-lg font-descriptions font-medium
+          truncate text-zinc-700 dark:text-zinc-200"
+        >
           {data.name || 'Not Available'}
+        </h2>
+        </div>
+        <div className="h-auto w-[20%] text-right">
+          <div className="h-auto w-auto">
+            <h2 className={`text-base font-descriptions font-medium
+              ${data.type === 'Income' && 'text-green-600'} 
+              ${data.type === 'Expense' && 'text-red-600'} 
+              ${data.type === 'Savings' && 'text-blue-600'} 
+              `}
+            >
+              {data.type || 'Note Available'}
+            </h2>
+          </div>
+        </div>
+      </div>
+      <div className="h-auto w-full text-left">
+        <h2 className={`text-base font-descriptions font-medium
+          ${data.type === 'Income' && 'text-green-600'}
+          ${data.type === 'Expense' && 'text-red-600'}
+          ${data.type === 'Savings' && 'text-blue-600'}
+          `}
+        >
+          {`${data.amount || 0} BDT`}
         </h2>
       </div>
       <div className="h-auto w-full flex justify-between items-center">
-        <div className="h-auto w-auto">
-          <h2 className={`text-base font-descriptions font-medium
-           ${data.type === 'Expense' ? 'text-red-600' : data.type === 'Income' ?
-           'text-green-600' : data.type === 'Savings' ? 'text-blue-600' : ''}  `}>
-            {`${data.amount || '0000'} BDT`}
-          </h2>
-        </div>
-        <div className="h-auto w-auto">
-          <h2 className={`text-base font-descriptions font-medium
-           ${data.type === 'Expense' ? 'text-red-600' : data.type === 'Income' ?
-           'text-green-600' : data.type === 'Savings' ? 'text-blue-600' : ''}  `}>
-            {data.type || 'Note Available'}
-          </h2>
-        </div>
-      </div>
-        <div className="h-auto w-full flex justify-between items-center">
           <div className="h-auto w-auto">
             <h2 className="text-base text-zinc-200 font-descriptions font-medium">
               {data.date || 'Note Available'}
             </h2>
           </div>
           <div className="h-auto w-auto">
-            <button type="button" className="cursor-pointer"
-            onClick={() => removeTransaction(data.$id)} >
-              <Trash2 size={22} className="text-red-600" />
+            <button 
+              onClick={() => removeTransaction(data.$id)} 
+              className="px-3 py-1 text-base font-descriptions font-medium 
+              rounded cursor-pointer text-white bg-red-600"
+            >
+              Remove
             </button>
           </div>
-        </div>
+      </div>
     </div>
   )
 }
