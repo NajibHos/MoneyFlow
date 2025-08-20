30eea303e5592c25b28eafc4d6ee3ef77068a58a
Najib Hossain <najibhossn@gmail.com>
2025-08-20 20:38:58 +0600

    Update toast position

--- src/components/Toast.jsx
+++ src/components/Toast.jsx
@@ -1,14 +1,13 @@
 
 const Toast = (props) => {
   return (
-    <div className="h-auto w-auto">
-      <div className="toast toast-top toast-center mt-12">
-        <div className={`alert ${props.data.bg}`}>
-        <span className="text-white text-base font-medium
-         font-descriptions">
-          { props.data.title }
-        </span>
-        </div>
+    <div className="toast toast-top toast-end">
+      <div className={`alert ${props.data.bg}`}>
+      <span className="text-white text-base font-medium
+        font-descriptions"
+      >
+        {props.data.title}
+      </span>
       </div>
     </div>
   )
