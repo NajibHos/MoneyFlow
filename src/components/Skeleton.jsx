3b987017f1cb341294c2c66fb3a454b784354c0c
Najib Hossain <najibhossn@gmail.com>
2025-08-20 19:02:19 +0600

    Add theme style for skeleton background

--- src/components/Skeleton.jsx
+++ src/components/Skeleton.jsx
@@ -2,11 +2,12 @@
 const Skeleton = () => {
   return (
     <div className="h-[90vh] w-full flex justify-center items-center">
-      <div className="h-full w-[90%] lg:max-2xl:w-[60%]
-      flex flex-col justify-center items-center gap-6">
-        <div className="skeleton h-6 w-full"></div>
-        <div className="skeleton h-6 w-full"></div>
-        <div className="skeleton h-6 w-full"></div>
+      <div className="h-full w-[90%] lg:max-2xl:w-[60%] flex flex-col 
+        justify-center items-center gap-6"
+      >
+        <div className="skeleton h-6 w-full bg-card-light dark:bg-card-dark"></div>
+        <div className="skeleton h-6 w-full bg-card-light dark:bg-card-dark"></div>
+        <div className="skeleton h-6 w-full bg-card-light dark:bg-card-dark"></div>
       </div>
     </div>
   )
