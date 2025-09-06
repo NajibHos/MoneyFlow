8d385005251fa15b190a4587ec4e75cea9f614d8
Najib Hossain <najibhossn@gmail.com>
2025-09-06 22:31:06 +0600

    Fix header element spacing, wrap elements with parents, update conditional rendering logic

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,12 +7,11 @@ const Header = () => {
 
   const { user, logOutUser } = useAuth();
   const { theme, toggleTheme } = useTheme();
-  console.log(theme)
 
   return (
      <div className="h-auto w-full py-5 flex justify-center items-center">
       <div className="h-full w-[90%] flex justify-between items-center">
-        <div className="h-auto w-[40%] flex justify-start items-center">
+        <div className="h-auto w-auto flex justify-start items-center">
           <Link to='/'>
           <h2 className="text-3xl font-headings font-medium
             cursor-pointer text-zinc-900 dark:text-white"
@@ -21,7 +20,7 @@ const Header = () => {
           </h2>
           </Link>
         </div>
-        <div className="h-auto w-[40%] flex justify-end items-center gap-5 md:gap-4">
+        <div className="h-auto w-auto flex justify-end-safe items-center gap-5">
           {
             theme === 'light' && <button 
               onClick={toggleTheme}
@@ -38,50 +37,58 @@ const Header = () => {
               <Moon size={22} />
             </button>
           }
-            {
-            !user ? (
-              <> 
-              {/* 2 different buttons for smaller and larger screens */}
-              <Link to='/sign-in'>
-              <button 
-                className="block md:hidden cursor-pointer text-zinc-900 
-                dark:text-white hover:text-blue-600"
-              >
-                <LogIn size={22} />
-              </button>
-              </Link>
-
-              <Link to='/sign-in'>
-              <button 
-                className="hidden md:block
-                text-lg font-descriptions font-medium cursor-pointer
-                text-zinc-900 dark:text-white hover:text-blue-600"
-              >
-                Sign in
-              </button>
-              </Link>
-              </>
-            ) : (
-              <>
-                {/* 2 different buttons for smaller and larger screens */}
-                <Link to='/dashboard'>
-                <button
+          {
+            !user && <> 
+              <div className="h-auto w-auto">
+                <Link to='/sign-in'>
+                <button 
                   className="block md:hidden cursor-pointer text-zinc-900 
                   dark:text-white hover:text-blue-600"
+                >
+                  <LogIn size={22} />
+                </button>
+                </Link>
+              </div>
+
+              <div className="h-auto w-auto">
+                <Link to='/sign-in'>
+                <button 
+                  className="hidden md:block
+                  text-lg font-descriptions font-medium cursor-pointer
+                  text-zinc-900 dark:text-white hover:text-blue-600"
+                >
+                  Sign in
+                </button>
+                </Link>
+              </div>
+            </>
+          } 
+          {
+            user && <>
+              <div className="h-auto w-auto">
+                <Link to='/dashboard'>
+                <button
+                  className="block md:hidden 
+                  cursor-pointer text-zinc-900 dark:text-white hover:text-blue-600"
                 >
                   <House size={22} />
                 </button>
                 </Link>
-
+              </div>
+               
+              <div className="h-auto w-auto">
                 <Link to='/dashboard'>
                 <button
                   className="hidden md:block
                   text-lg font-descriptions font-medium cursor-pointer
-                  text-zinc-900 dark:text-white hover:text-blue-600">
+                  text-zinc-900 dark:text-white hover:text-blue-600"
+                >
                   Dashboard
                 </button>
                 </Link>
-
+              </div>
+                
+              <div className="h-auto w-auto">
                 <button 
                   className="block md:hidden cursor-pointer text-zinc-900 
                   dark:text-white hover:text-blue-600"
@@ -89,7 +96,9 @@ const Header = () => {
                 >
                   <LogOut size={22} />
                 </button>
+              </div>
 
+              <div className="h-auto w-auto">
                 <button
                   onClick={logOutUser}
                   className="hidden md:block
@@ -98,8 +107,8 @@ const Header = () => {
                 >
                   Sign Out
                 </button>
-              </>
-            )
+              </div>
+            </>
           }
         </div>
       </div>
