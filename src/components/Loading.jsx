f939ddddd872483424bcef19a5806694590c98e6
Najib Hossain <najibhossn@gmail.com>
2025-09-07 11:16:13 +0600

    Replace skeleton with loading screen

---
+++ src/components/Loading.jsx
@@ -0,0 +1,10 @@
+
+const Loading = () => {
+  return (
+    <div className="h-[90vh] w-full flex justify-center items-center">
+      <span className="loading loading-dots loading-xl bg-zinc-900 dark:bg-white"></span>
+    </div>
+  )
+}
+
+export default Loading
