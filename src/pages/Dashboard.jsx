a186d935f8b441bb49014c75c206e03b66c519bb
Najib Hossain <najibhossn@gmail.com>
2025-10-06 09:49:20 +0600

    Update transaction logic with React useRef hook

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,35 +1,24 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { DatabaseID, databases, DBCollectionID } from "../appwrite/Appwrite";
 import { useAuth } from "../utils/AuthProvider";
 import { useNavigate } from "react-router-dom";
 
 const Dashboard = () => {
 
-  const [transactions, setTransactions] = useState([]);
   const { user, userName } = useAuth();
+  const [transactions, setTransactions] = useState([]);
   const navigate = useNavigate();
+  // transaction amount references
+  const income = useRef(0);
+  const expense = useRef(0);
+  const savings = useRef(0);
+  const balance = useRef(0);
 
   useEffect(() => {
     getTransactions();
   }, [user])
 
-  //transaction amount and type for tracking total transactions amount
-  const funds = [
-    {
-      type: 'Income',
-      amount: 0
-    },
-    {
-      type: 'Expense',
-      amount: 0
-    },
-    {
-      type: 'Savings',
-      amount: 0
-    },
-  ]
-
-  //fetching transaction data
+  // fetch transaction data
   const getTransactions = async () => {
     try {
       const data = await databases.listDocuments(
@@ -44,37 +33,32 @@ const Dashboard = () => {
     }
   }
 
-  //re-assigning transaction amount variables
-  transactions.forEach(( value ) => {
+  // calculate total amounts
+  income.current = transactions
+    .filter((item) => item.type === 'Income')
+    .reduce((acc, item) => acc + item.amount, 0);
 
-    switch (value.type) {
-      case 'Income' :
-        funds[0].amount += value.amount; //Income
-        break;
-      case 'Expense' :
-        funds[1].amount += value.amount; //Expense
-        break;
-      case 'Savings' :
-        funds[2].amount += value.amount; //Savings
-        break;
+  expense.current = transactions
+    .filter((item) => item.type === 'Expense')
+    .reduce((acc, item) => acc + item.amount, 0);
 
-      default:
-        ''
-        break;
-    }
+  savings.current = transactions
+    .filter((item) => item.type === 'Savings')
+    .reduce((acc, item) => acc + item.amount, 0);
 
-  });
-
-  // available balance
-  let balance = funds[0].amount - (funds[1].amount + funds[2].amount);
+  // available balance calculation
+  balance.current = income.current - (expense.current + savings.current);
+  let availableBalance = balance.current;
 
   return (
-    <div className="h-auto w-full py-12 lg:h-[90vh] lg:py-0 flex justify-center items-center">
+    <div className="h-auto w-full py-12 lg:h-[90vh] lg:py-0 flex
+      justify-center items-center"
+    >
       <div className="h-full w-[90%] flex flex-col justify-center
         items-center gap-16 lg:gap-14"
       >
         <div className="h-auto w-full text-center">
-          <h2 className="text-2xl font-descriptions font-medium 
+          <h2 className="text-2xl font-descriptions font-medium
             text-zinc-800 dark:text-zinc-200"
           >
             Dashboard
@@ -87,7 +71,7 @@ const Dashboard = () => {
             <h2 className="text-base font-descriptions font-medium
               text-zinc-700 dark:text-zinc-300"
             >
-              Welcome <span 
+              Welcome <span
                 className="text-zinc-900 dark:text-white"
                 >
                   {userName || null}
@@ -105,16 +89,16 @@ const Dashboard = () => {
             <h2 className="text-base font-descriptions font-medium
               text-zinc-700 dark:text-zinc-300"
             >
-              Your remaining balance is: 
+              Your remaining balance is:
               <span className="text-lg ml-1 text-green-600">
-                {`${balance || 0} BDT`}
+                {`${availableBalance || 0} BDT`}
               </span>
             </h2>
           </div>
         </div>
         <div className="h-auto w-full grid grid-cols-1 lg:grid-cols-3 gap-8">
-          <div className="h-auto w-full p-6 flex flex-col justify-center items-center gap-5
-            rounded bg-card-light dark:bg-card-dark"
+          <div className="h-auto w-full p-6 flex flex-col justify-center
+            items-center gap-5 rounded bg-card-light dark:bg-card-dark"
           >
             <div className="h-auto w-full text-left">
               <h2 className="text-base font-descriptions font-medium
@@ -127,10 +111,12 @@ const Dashboard = () => {
               <h2 className="text-xl font-descriptions font-medium
                 text-green-600 dark:text-white"
               >
-                {`+ ${funds[0].amount || 0} BDT`}
+                {`+ ${income.current || 0} BDT`}
               </h2>
             </div>
-            <div className="h-auto w-full flex justify-start items-center gap-5">
+            <div className="h-auto w-full flex justify-start items-center
+              gap-5"
+            >
               <div className="h-auto w-auto">
                 <button
                   onClick={() => navigate('/add-income')}
@@ -156,8 +142,8 @@ const Dashboard = () => {
               </div>
             </div>
           </div>
-          <div className="h-auto w-full p-6 flex flex-col justify-center items-center gap-5
-            rounded bg-card-light dark:bg-card-dark"
+          <div className="h-auto w-full p-6 flex flex-col justify-center
+            items-center gap-5 rounded bg-card-light dark:bg-card-dark"
           >
             <div className="h-auto w-full text-left">
               <h2 className="text-base font-descriptions font-medium
@@ -170,10 +156,12 @@ const Dashboard = () => {
               <h2 className="text-xl font-descriptions font-medium
                 text-red-600 dark:text-white"
               >
-                {`- ${funds[1].amount || 0} BDT`}
+                {`- ${expense.current || 0} BDT`}
               </h2>
             </div>
-            <div className="h-auto w-full flex justify-start items-center gap-5">
+            <div className="h-auto w-full flex justify-start items-center
+              gap-5"
+            >
               <div className="h-auto w-auto">
                 <button
                   onClick={() => navigate('/add-expense')}
@@ -199,8 +187,8 @@ const Dashboard = () => {
               </div>
             </div>
           </div>
-          <div className="h-auto w-full p-6 flex flex-col justify-center items-center gap-5
-            rounded bg-card-light dark:bg-card-dark"
+          <div className="h-auto w-full p-6 flex flex-col justify-center
+            items-center gap-5 rounded bg-card-light dark:bg-card-dark"
           >
             <div className="h-auto w-full text-left">
               <h2 className="text-base font-descriptions font-medium
@@ -213,10 +201,12 @@ const Dashboard = () => {
               <h2 className="text-xl font-descriptions font-medium
                 text-blue-600 dark:text-white"
               >
-                {`+ ${funds[2].amount || 0} BDT`}
+                {`+ ${savings.current || 0} BDT`}
               </h2>
             </div>
-            <div className="h-auto w-full flex justify-start items-center gap-5">
+            <div className="h-auto w-full flex justify-start items-center
+              gap-5"
+            >
               <div className="h-auto w-auto">
                 <button
                   onClick={() => navigate('/add-savings')}
